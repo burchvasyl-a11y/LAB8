1461b91c6470b4aeed28ca6cd01f7fa1f718691c
burchvasyl-a11y <c.burch.vasyl@student.uzhnu.edu.ua>
2026-03-13 15:56:52 +0200

    Add files via upload

---
+++ LAB8/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+namespace LAB8
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MovingButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // MovingButton
+            // 
+            this.MovingButton.Location = new System.Drawing.Point(328, 189);
+            this.MovingButton.Name = "MovingButton";
+            this.MovingButton.Size = new System.Drawing.Size(102, 44);
+            this.MovingButton.TabIndex = 0;
+            this.MovingButton.Text = "MovingButton";
+            this.MovingButton.UseVisualStyleBackColor = true;
+            this.MovingButton.Click += new System.EventHandler(this.MovingButton_Click);
+            // 
+            // ButtonAnimation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MovingButton);
+            this.Name = "ButtonAnimation";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button MovingButton;
+    }
+}
+
---
+++ LAB8/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB8
+{
+    public partial class MainForm : Form
+    {
+        private Timer AnimationTimer;
+        private int targetX;
+        private const int speed = 10;
+
+        public MainForm()
+        {
+            this.Size = new Size(600, 300);
+            this.Text = "Анімація руху кнопки";
+
+            // Створюємо кнопку
+            MovingButton = new Button();
+            MovingButton.Text = "Рухай мене";
+            MovingButton.Size = new Size(120, 40);
+            MovingButton.Location = new Point(10, 100);
+            MovingButton.Click += MovingButton_Click;
+            this.Controls.Add(MovingButton);
+
+            AnimationTimer = new Timer();
+            AnimationTimer.Interval = 40;
+            AnimationTimer.Tick += AnimationTimer_Tick;
+        }
+
+        private void MovingButton_Click(object sender, EventArgs e)
+        {
+            if (MovingButton.Left < this.ClientSize.Width / 2)
+                targetX = this.ClientSize.Width - MovingButton.Width - 10;
+            else
+                targetX = 10;
+
+            AnimationTimer.Start();
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            if (MovingButton.Left < targetX)
+            {
+                MovingButton.Left += speed;
+                if (MovingButton.Left >= targetX)
+                {
+                    MovingButton.Left = targetX;
+                    AnimationTimer.Stop();
+                }
+            }
+            else if (MovingButton.Left > targetX)
+            {
+                MovingButton.Left -= speed;
+                if (MovingButton.Left <= targetX)
+                {
+                    MovingButton.Left = targetX;
+                    AnimationTimer.Stop();
+                }
+            }
+        }
+    }
+}
